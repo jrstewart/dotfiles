122a550fd9f373d58e07b486c99ac792dd5b132a
Jason Stewart <jason.r.stewart@gmail.com>
2021-12-02 11:48:33 -0600

    Formatting updates for the nvim configuration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,7 +25,7 @@ return require('packer').startup(function()
     'nvim-telescope/telescope.nvim',
     requires = {'nvim-lua/plenary.nvim'}
   }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ":TSUpdate"
