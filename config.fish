7fcc15b37be06b5c2484a6265fb8d540467d7874
Jason Stewart <jason.r.stewart@gmail.com>
2022-06-06 14:41:54 -0500

    Add the fish shell config file

---
+++ config.fish
@@ -0,0 +1,26 @@
+set -g -x fish_greeting ""
+
+set -xU ERL_AFLAGS "-kernel shell_history enabled"
+
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_PAGER=""
+export CHROME_EXECUTABLE="google-chrome-stable"
+
+# Ruby aliases
+alias b='bundle'
+alias be='bundle exec'
+
+# Tmux aliases
+alias tma='tmux attach -t'
+alias tml='tmux ls'
+alias tmn='tmux new -s'
+
+# Docker aliases
+alias dcb='docker-compose build'
+alias dcr='docker-compose run'
+alias dcs='docker-compose start'
+alias dcp='docker-compose stop'
+alias dcu='docker-compose up'
+alias dcl='docker-compose logs'
+
+source /opt/asdf-vm/asdf.fish
