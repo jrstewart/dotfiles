7f40280b11df1da90756a58cc59eda8aad23a45c
Jason Stewart <jason.r.stewart@gmail.com>
2023-09-07 10:25:57 -0500

    Update the fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -11,24 +11,26 @@ export CHROME_EXECUTABLE="google-chrome-stable"
 
 # System aliases
 alias grep="rg"
-alias ls='exa --icons'
-alias ll='exa -l --icons'
+alias ls="exa --icons"
+alias ll="exa -l --icons"
 
 # Ruby aliases
-alias b='bundle'
-alias be='bundle exec'
+alias b="bundle"
+alias be="bundle exec"
 
 # Tmux aliases
-alias tma='tmux attach -t'
-alias tml='tmux ls'
-alias tmn='tmux new -s'
+alias tma="tmux attach -t"
+alias tml="tmux ls"
+alias tmn="tmux new -s"
 
 # Docker aliases
-alias dcb='docker-compose build'
-alias dcr='docker-compose run'
-alias dcs='docker-compose start'
-alias dcp='docker-compose stop'
-alias dcu='docker-compose up'
-alias dcl='docker-compose logs'
+alias dcb="docker-compose build"
+alias dcr="docker-compose run"
+alias dcs="docker-compose start"
+alias dcp="docker-compose stop"
+alias dcu="docker-compose up"
+alias dcl="docker-compose logs"
 
 source ~/.asdf/asdf.fish
+
+direnv hook fish | source
