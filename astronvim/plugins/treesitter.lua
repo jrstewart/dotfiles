1be4287e9db6f696101ebf2f03b59b1123d1dcd6
Jason Stewart <jason.r.stewart@gmail.com>
2023-09-07 10:27:13 -0500

    Add the AstroNvim configuration

---
+++ astronvim/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+      "css",
+      "dockerfile",
+      "elixir",
+      "erlang",
+      "fish",
+      "gitignore",
+      "graphql",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "python",
+      "ruby",
+      "rust",
+      "sql",
+      "typescript",
+      "yaml",
+    })
+  end,
+}
